344f6ab8b59a229aa2bb2df6c069103390e7504e
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-02-20 19:42:55 +0300

    scripts

---
+++ scripts/anim.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+mkdir pics
+i=$1
+echo "$i"
+for i in *.txt
+do
+	echo -e "set view 1,360\n \
+	set terminal png\n \
+	unset surface \n \
+	set cntrparam levels 20 \n \
+	set contour \n \
+	splot \"$i\" matrix with lines" | gnuplot > pics/$i.png
+done
