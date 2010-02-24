963d4a405763eb138aab585086dc088c97e87bc2
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-02-24 14:32:21 +0300

    fine output

--- src/test_barvortex.cpp
+++ src/test_barvortex.cpp
@@ -138,6 +138,43 @@ double test_coriolis (double phi, double lambda)
 	return 2 * sin(phi) + 0.5 * cos(2 * lambda) * sign(2 * phi) * ipow(sin(2 * phi), 2);
 }
 
+void output_psi(const char * prefix, const char * suffix,
+				const double * psi, long nlat, long nlon,
+				double U0, double PSI0,
+				SphereGrad & grad)
+{
+	vector < double > u (nlat * nlon);
+	vector < double > v (nlat * nlon);
+	vector < double > Psi (nlat * nlon);
+
+	grad.calc(&u[0], &v[0], &psi[0]);
+
+	vec_mult_scalar(&u[0], &u[0], -1.0, nlat * nlon);
+
+	char ubuf[1024]; char vbuf[1024]; char psibuf[1024];
+	char Ubuf[1024]; char Vbuf[1024]; char Psibuf[1024];
+
+	snprintf(ubuf, 1024,   "out/%snorm_u%s.txt", prefix, suffix);
+	snprintf(vbuf, 1024,   "out/%snorm_v%s.txt", prefix, suffix);
+	snprintf(psibuf, 1024, "out/%snorm_psi%s.txt", prefix, suffix);
+
+	snprintf(Ubuf, 1024,   "out/%sorig_u%s.txt", prefix, suffix);
+	snprintf(Vbuf, 1024,   "out/%sorig_v%s.txt", prefix, suffix);
+	snprintf(Psibuf, 1024, "out/%sorig_psi%s.txt", prefix, suffix);
+
+	fprintfwmatrix(ubuf,   &u[0], nlat, nlon, "%23.16lf ");
+	fprintfwmatrix(vbuf,   &v[0], nlat, nlon, "%23.16lf ");
+	fprintfwmatrix(psibuf, &psi[0], nlat, nlon, "%23.16lf ");
+
+	vec_mult_scalar(&u[0], &u[0], U0, nlon * nlat);
+	vec_mult_scalar(&v[0], &v[0], U0, nlon * nlat);
+	vec_mult_scalar(&Psi[0],  &psi[0],  PSI0, nlon * nlat);
+
+	fprintfwmatrix(Ubuf,   &u[0], nlat, nlon, "%23.16le ");
+	fprintfwmatrix(Vbuf,   &v[0], nlat, nlon, "%23.16le ");
+	fprintfwmatrix(Psibuf, &Psi[0], nlat, nlon, "%23.16le ");
+}
+
 void run_test(const char * srtm)
 {
 	long nlat =  3 * 19;
@@ -166,14 +203,11 @@ void run_test(const char * srtm)
 
 	vector < double > u (nlat * nlon);
 	vector < double > v (nlat * nlon);
-	vector < double > U (nlat * nlon);
+
 	vector < double > r (nlat * nlon);
 	vector < double > f (nlat * nlon);
 	vector < double > cor(nlat * nlon);
 	vector < double > rel(nlat * nlon);
-
-	vector < double > uu (nlat * nlon);
-	vector < double > vv (nlat * nlon);
 
 	double nr = 0;
 	double omg = 2.*M_PI/24./60./60.; // ?
@@ -263,35 +297,17 @@ void run_test(const char * srtm)
 	while (t < T)
 	{
 		if (it % part_of_the_day == 0) {
+			char buf[1024];
 			nr = bv.norm(&r[0]);
 			fprintf(stderr, "nr=%.16lf, t=%.16lf of %.16lf\n", nr, t, T);
+			snprintf(buf, 1024, "%06d", it);
+			output_psi("", buf, &r[0], nlat, nlon, U0, PSI0, grad);
 
-			grad.calc(&uu[0], &vv[0], &r[0]);
+			vector < double > m = var.m_current();
+			vector < double > d = var.current();
 
-			vec_mult_scalar(&uu[0], &uu[0], -1.0, nlat * nlon);
-
-			char ubuf[1024]; char vbuf[1024]; char psibuf[1024];
-			char Ubuf[1024]; char Vbuf[1024]; char Psibuf[1024];
-
-			snprintf(ubuf, 1024,   "out/norm_u_%06d.txt", it);
-			snprintf(vbuf, 1024,   "out/norm_v_%06d.txt", it);
-			snprintf(psibuf, 1024, "out/norm_psi_%06d.txt", it);
-
-			snprintf(Ubuf, 1024,   "out/orig_u_%06d.txt", it);
-			snprintf(Vbuf, 1024,   "out/orig_v_%06d.txt", it);
-			snprintf(Psibuf, 1024, "out/orig_psi_%06d.txt", it);
-
-			fprintfwmatrix(ubuf,   &uu[0], nlat, nlon, "%23.16lf ");
-			fprintfwmatrix(vbuf,   &vv[0], nlat, nlon, "%23.16lf ");
-			fprintfwmatrix(psibuf,  &u[0], nlat, nlon, "%23.16lf ");
-
-			vec_mult_scalar(&uu[0], &uu[0], U0, nlon * nlat);
-			vec_mult_scalar(&vv[0], &vv[0], U0, nlon * nlat);
-			vec_mult_scalar(&U[0],  &u[0],  PSI0, nlon * nlat);
-
-			fprintfwmatrix(Ubuf,   &uu[0], nlat, nlon, "%23.16le ");
-			fprintfwmatrix(Vbuf,   &vv[0], nlat, nlon, "%23.16le ");
-			fprintfwmatrix(Psibuf,  &U[0], nlat, nlon, "%23.16le ");
+			output_psi("m_", "", &m[0], nlat, nlon, U0, PSI0, grad);
+			output_psi("d_", "", &d[0], nlat, nlon, U0, PSI0, grad);
 		}
 
 		bv.S_step (&u[0], &r[0], t);
@@ -308,8 +324,8 @@ void run_test(const char * srtm)
 		vector < double > m = var.m_current();
 		vector < double > d = var.current();
 
-		fprintfwmatrix("out/m.txt", &m[0], nlat, nlon, "%23.16lf ");
-		fprintfwmatrix("out/d.txt", &d[0], nlat, nlon, "%23.16lf ");
+		output_psi("m_", "", &m[0], nlat, nlon, U0, PSI0, grad);
+		output_psi("d_", "", &d[0], nlat, nlon, U0, PSI0, grad);
 	}
 }
 
