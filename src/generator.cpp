bff792eb7d04739ccdacaf8473f47f5c5059aba8
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-03-07 22:32:32 +0300

    fix

---
+++ src/generator.cpp
@@ -0,0 +1,71 @@
+#include <stdexcept>
+#include <sstream>
+#include "generator.h"
+
+using namespace std;
+
+Parser::Parser(): 
+	error(false), 
+	precission(0),
+	method(0)
+{
+}
+
+Parser::~Parser()
+{
+}
+
+void Parser::set_name(const std::string & n)
+{
+	name = n;
+}
+
+void Parser::set_precission(const std::string & p)
+{
+	if (p == "float") {
+		precission = FLOAT;
+	} else if (p == "double") {
+		precission = DOUBLE;
+	} else if (p == "both") {
+		precission = BOTH;
+	} else {
+		std::stringstream str;
+		str << "unknown precission " << p;
+		throw std::logic_error(str.str());
+	}
+}
+
+void Parser::set_method(const std::string & p)
+{
+	if (p == "harmonic") {
+		method = HARMONIC;
+	} else {
+		std::stringstream str;
+		str << "unknown method " << p;
+		throw std::logic_error(str.str());
+	}
+}
+
+void Parser::set_surface(const std::string & p)
+{
+}
+
+void Parser::set_approximation(const std::string &p)
+{
+}
+
+void Parser::add_map(const std::string & from, const std::string & to)
+{
+}
+
+void Parser::add_scalar(const std::string &p)
+{
+}
+
+void Parser::add_function(const std::string &p, int args)
+{
+}
+
+void Parser::new_equation()
+{
+}
--- src/generator.h
+++ src/generator.h
@@ -1,9 +1,49 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
 
+#include <string>
 struct Parser
 {
 	bool error;
+	std::string name;
+
+	enum {
+		FLOAT = 1,
+		DOUBLE = 2,
+		BOTH = 3,
+	};
+
+	int precission;
+
+	enum {
+		FEM = 1,
+		FDM = 2,
+		HARMONIC = 3,
+	};
+
+	int method;
+
+	enum {
+		FLAT = 1,
+		SPHERE = 2,
+	};
+
+	int surface;
+
+	Parser();
+	~Parser();
+
+	void set_name(const std::string & n);
+	void set_precission(const std::string & p);
+	void set_method(const std::string &p);
+	void set_surface(const std::string &p);
+	void set_approximation(const std::string &p);
+
+	void add_map(const std::string & from, const std::string & to);
+	void add_scalar(const std::string &p);
+	void add_function(const std::string &p, int args);
+
+	void new_equation();
 };
 
 #endif /* GENERATOR_H */
