c3c37e98e71f8bfbbc035302be380d638f7d8689
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-03-07 23:42:25 +0300

    generators factory

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,13 +1,80 @@
 #include <stdexcept>
 #include <sstream>
+#include <map>
 #include "generator.h"
 
 using namespace std;
 
-Parser::Parser(): 
-	error(false), 
-	precission(0),
-	method(0)
+class Registrator
+{
+	Registrator() {}
+	~Registrator() {}
+
+public:
+	map < string, generator_creator_t > generators;
+
+	static Registrator & instance()
+	{
+		static Registrator r;
+		return r;
+	}
+};
+
+Generator::Generator(): precission(0)
+{
+}
+
+Generator::~Generator()
+{
+}
+
+int register_generator(const std::string & name, generator_creator_t creator)
+{
+	Registrator::instance().generators.insert(make_pair(name, creator));
+	return 1;
+}
+
+Generator * make_generator(const std::string & name)
+{
+	map < string, generator_creator_t > & generators = Registrator::instance().generators;
+	if (generators.find(name) != generators.end()) {
+		return generators[name]();
+	}
+	return 0;
+}
+
+void Generator::set_precission(const std::string & p)
+{
+	if (p == "double") {
+		precission = DOUBLE;
+	} else if (p == "float") {
+		precission = FLOAT;
+	} else if (p == "both") {
+		precission = BOTH;
+	} else {
+		stringstream str;
+		str << "unknown precission " << p;
+		throw std::logic_error(str.str());
+	}
+}
+
+void Generator::add_map(const std::string & from, const std::string & to)
+{
+}
+
+void Generator::add_scalar(const std::string &p)
+{
+}
+
+void Generator::add_function(const std::string &p, int args)
+{
+}
+
+void Generator::new_equation()
+{
+}
+
+Parser::Parser()
 {
 }
 
@@ -20,26 +87,31 @@ void Parser::set_name(const std::string & n)
 	name = n;
 }
 
+void Parser::check(const string &p)
+{
+	if (!generator.get())
+	{
+		stringstream str;
+		str << p << ": declare method first ! ";
+		throw logic_error(str.str());
+	}
+}
+
 void Parser::set_precission(const std::string & p)
 {
-	if (p == "float") {
-		precission = FLOAT;
-	} else if (p == "double") {
-		precission = DOUBLE;
-	} else if (p == "both") {
-		precission = BOTH;
-	} else {
-		std::stringstream str;
-		str << "unknown precission " << p;
-		throw std::logic_error(str.str());
-	}
+	check("precission");
+	generator->set_precission(p);
 }
 
 void Parser::set_method(const std::string & p)
 {
-	if (p == "harmonic") {
-		method = HARMONIC;
-	} else {
+	if (generator.get()) {
+		throw std::logic_error("method already set");
+	}
+
+	generator.reset(make_generator(p));
+
+	if (!generator.get()) {
 		std::stringstream str;
 		str << "unknown method " << p;
 		throw std::logic_error(str.str());
@@ -48,24 +120,36 @@ void Parser::set_method(const std::string & p)
 
 void Parser::set_surface(const std::string & p)
 {
+	check("surface");
+	generator->set_surface(p);
 }
 
 void Parser::set_approximation(const std::string &p)
 {
+	check("approximation");
+	generator->set_approximation(p);
 }
 
 void Parser::add_map(const std::string & from, const std::string & to)
 {
+	check("map");
+	generator->add_map(from, to);
 }
 
 void Parser::add_scalar(const std::string &p)
 {
+	check("scalar");
+	generator->add_scalar(p);
 }
 
 void Parser::add_function(const std::string &p, int args)
 {
+	check("function");
+	generator->add_function(p, args);
 }
 
 void Parser::new_equation()
 {
+	check("equation");
+	generator->new_equation();
 }
--- src/generator.h
+++ src/generator.h
@@ -2,11 +2,16 @@
 #define GENERATOR_H
 
 #include <string>
-struct Parser
-{
-	bool error;
-	std::string name;
+#include <map>
+#include <memory>
 
+struct Generator;
+typedef Generator * (*generator_creator_t)(void);
+int register_generator(const std::string & name, generator_creator_t);
+Generator * make_generator(const std::string & name);
+
+struct Generator
+{
 	enum {
 		FLOAT = 1,
 		DOUBLE = 2,
@@ -15,27 +20,33 @@ struct Parser
 
 	int precission;
 
-	enum {
-		FEM = 1,
-		FDM = 2,
-		HARMONIC = 3,
-	};
+	Generator();
+	virtual ~Generator();
 
-	int method;
+	virtual void set_surface(const std::string &p) = 0;
+	virtual void set_approximation(const std::string &p) = 0;
+	virtual void set_precission(const std::string & p);
 
-	enum {
-		FLAT = 1,
-		SPHERE = 2,
-	};
+	virtual void add_map(const std::string & from, const std::string & to);
+	virtual void add_scalar(const std::string &p);
+	virtual void add_function(const std::string &p, int args);
 
-	int surface;
+	virtual void new_equation();
+};
+
+struct Parser
+{
+	std::string name;
+
+	std::auto_ptr < Generator > generator;
 
 	Parser();
 	~Parser();
 
-	void set_name(const std::string & n);
-	void set_precission(const std::string & p);
 	void set_method(const std::string &p);
+	void set_name(const std::string & n);
+
+	void set_precission(const std::string & p);
 	void set_surface(const std::string &p);
 	void set_approximation(const std::string &p);
 
@@ -44,6 +55,10 @@ struct Parser
 	void add_function(const std::string &p, int args);
 
 	void new_equation();
+
+	void check(const std::string &p);
 };
+
+#include "generator_harmonic.h"
 
 #endif /* GENERATOR_H */
