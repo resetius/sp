dc1abb97b7a8e8fba39c94676733cfd3915fdccb
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-06-26 17:01:35 +0400

    baroclin real calc

--- src/test_baroclin.cpp
+++ src/test_baroclin.cpp
@@ -5,10 +5,17 @@
 #include <math.h>
 
 #include "baroclin.h"
+#include "grad.h"
+#include "vorticity.h"
+#include "statistics.h"
 #include "linal.h"
 
 using namespace std;
 using namespace linal;
+
+#ifdef WIN32
+#define snprintf _snprintf
+#endif
 
 double u1_t (double x, double y, double t)
 {
@@ -203,7 +210,44 @@ void proj(double * u, SphereBaroclin & bv, SphereBaroclin::Conf & conf, T rp, do
 	}
 }
 
-void test_barvortex()
+void output_psi(const char * prefix, const char * suffix,
+				const double * psi, long nlat, long nlon,
+				double U0, double PSI0,
+				SphereGrad & grad)
+{
+	vector < double > u (nlat * nlon);
+	vector < double > v (nlat * nlon);
+	vector < double > Psi (nlat * nlon);
+
+	grad.calc(&u[0], &v[0], &psi[0]);
+
+	vec_mult_scalar(&u[0], &u[0], -1.0, nlat * nlon);
+
+	char ubuf[1024]; char vbuf[1024]; char psibuf[1024];
+	char Ubuf[1024]; char Vbuf[1024]; char Psibuf[1024];
+
+	snprintf(ubuf, 1024,   "out/%snorm_u%s.txt", prefix, suffix);
+	snprintf(vbuf, 1024,   "out/%snorm_v%s.txt", prefix, suffix);
+	snprintf(psibuf, 1024, "out/%snorm_psi%s.txt", prefix, suffix);
+
+	snprintf(Ubuf, 1024,   "out/%sorig_u%s.txt", prefix, suffix);
+	snprintf(Vbuf, 1024,   "out/%sorig_v%s.txt", prefix, suffix);
+	snprintf(Psibuf, 1024, "out/%sorig_psi%s.txt", prefix, suffix);
+
+	mat_print(ubuf,   &u[0], nlat, nlon, "%23.16lf ");
+	mat_print(vbuf,   &v[0], nlat, nlon, "%23.16lf ");
+	mat_print(psibuf, &psi[0], nlat, nlon, "%23.16lf ");
+
+	vec_mult_scalar(&u[0], &u[0], U0, nlon * nlat);
+	vec_mult_scalar(&v[0], &v[0], U0, nlon * nlat);
+	vec_mult_scalar(&Psi[0],  &psi[0],  PSI0, nlon * nlat);
+
+	mat_print(Ubuf,   &u[0], nlat, nlon, "%23.16le ");
+	mat_print(Vbuf,   &v[0], nlat, nlon, "%23.16le ");
+	mat_print(Psibuf, &Psi[0], nlat, nlon, "%23.16le ");
+}
+
+void test_convergence_barvortex()
 {
 	SphereBaroclin::Conf conf;
 	double R   = 6.371e+6;
@@ -292,6 +336,195 @@ void test_barvortex()
 	}
 }
 
+void real_calc(const char * srtm)
+{
+	long nlat = 5 * 19;
+	long nlon = 5 * 36;
+
+	SphereBaroclin::Conf conf;
+	conf.nlat     = nlat;
+	conf.nlon     = nlon;
+	conf.mu       = 1.5e-5;
+	conf.mu1      = conf.mu;
+	conf.sigma    = 1.14e-2;
+	conf.sigma1   = conf.sigma;
+	conf.alpha    = 1.0;
+	int part_of_the_day = 48;
+	conf.tau      = 2 * M_PI / (double) part_of_the_day;
+	conf.theta    = 0.5;
+	conf.k1       = 1.0;
+	conf.k2       = 1.0;
+	conf.rp1      = 0;
+	conf.rp2      = 0;
+	conf.cor      = 0;//test_coriolis;
+
+	double dlat = M_PI / (nlat - 1);
+	double dlon = 2. * M_PI / nlon;
+	double t = 0;
+	double T = 2 * M_PI * 1000.0;
+
+	int i, j, it = 0;
+
+	vector < double > u0 (nlat * nlon);
+	vector < double > v0 (nlat * nlon);
+
+	vector < double > u1 (nlat * nlon);
+	vector < double > v1 (nlat * nlon);
+
+	vector < double > psi0 (nlat * nlon);
+	vector < double > psi1 (nlat * nlon);
+
+	vector < double > psi0_n (nlat * nlon);
+	vector < double > psi1_n (nlat * nlon);
+
+	vector < double > f (nlat * nlon);
+	vector < double > cor(nlat * nlon);
+	vector < double > rel(nlat * nlon);
+
+	double nr0 = 0, nr1 = 0;
+	double omg = 2.*M_PI/24./60./60.; // ?
+	double TE  = 1./omg;
+	double RE  = 6.371e+6;
+	double PSI0 = RE * RE / TE;
+	double U0  = 6.371e+6/TE;
+	const char * fn = srtm ? srtm : "";
+
+	if (fn) {
+		FILE * f = fopen(fn, "rb");
+		if (f) {
+			size_t size = nlat * nlon * sizeof(double);
+			if (fread(&rel[0], 1, size, f) != size) {
+				fprintf(stderr, "bad relief file format ! \n");
+			}
+		} else {
+			fprintf(stderr, "relief file not found ! \n");
+		}
+		fclose(f);
+	}
+
+	double rel_max = 0.0;
+	for (i = 0; i < nlat * nlon; ++i) {
+		if (rel_max < rel[i]) rel_max = rel[i];
+		//if (rel_max < fabs(rel[i])) rel_max = fabs(rel[i]);
+	}
+
+	for (i = 0; i < nlat; ++i)
+	{
+		for (j = 0; j < nlon; ++j)
+		{
+			double phi    = -0.5 * M_PI + i * dlat;
+			double lambda = j * dlon;
+
+			//double ff = -(M_PI / 4 * ipow(phi, 2) - fabs(ipow(phi, 3)) / 3.0) * 16.0 / M_PI / M_PI * 3.0 / U0;
+			//r[i * nlon + j] = (phi > 0) ? ff : -ff;
+			if (phi > 0) {
+				u1[i * nlon + j] = (phi * (M_PI / 2. - phi) * 16 / M_PI / M_PI * 30.0 / U0);
+			} else {
+				u1[i * nlon + j] = (-phi * (M_PI / 2. + phi) * 16 / M_PI / M_PI * 30.0 / U0);
+			}
+			v1[i * nlon + j] = 0;
+			//cor[i * nlon + j] = conf.coriolis(phi, lambda);
+
+			//cor[i * nlon + j] = 1000 * rel[i * nlon + j] / rel_max + 2 * sin(phi);
+			//
+
+		//	rel[i * nlon + j] = 0.5 * sign(phi) * cos(2 * lambda) * ipow(sin(2 * phi), 2);
+
+			if (rel[i * nlon + j] > 0) {
+				rel[i * nlon + j] = 1.0 * rel[i * nlon + j] / rel_max;
+			} else {
+				rel[i * nlon + j] = 0.0;
+			}
+			cor[i * nlon + j] = rel[i * nlon + j] + 2 * sin(phi);
+		}
+	}
+
+	SphereOperator op(nlat, nlon, 0);
+	SphereLaplace lapl(op);
+	SphereGrad grad(op);
+	SphereVorticity vor(op);
+
+	vor.calc(&f[0], &u1[0], &v1[0]);
+	vor.test();
+	vec_mult_scalar(&f[0], &f[0], -1.0, nlat * nlon);
+
+	lapl.solve(&psi0[0], &f[0]);
+	vec_mult_scalar(&f[0], &f[0], conf.sigma, nlat * nlon);
+
+	conf.rp12 = 0;
+	conf.rp22 = &f[0];
+	conf.cor2 = &cor[0];
+
+	SphereBaroclin bv (conf);
+
+	Variance < double > var0(u0.size());
+	Variance < double > var1(u1.size());
+
+	mat_print("out/cor.txt", &cor[0], nlat, nlon, "%23.16lf ");
+	mat_print("out/rel.txt", &rel[0], nlat, nlon, "%23.16lf ");
+	mat_print("out/rp.txt", &f[0], nlat, nlon, "%23.16lf ");
+
+	mat_print("out/u0.txt", &u0[0], nlat, nlon, "%23.16lf ");
+	mat_print("out/v0.txt", &v0[0], nlat, nlon, "%23.16lf ");
+
+	mat_print("out/u1.txt", &u1[0], nlat, nlon, "%23.16lf ");
+	mat_print("out/v1.txt", &v1[0], nlat, nlon, "%23.16lf ");
+
+	while (t < T)
+	{
+		if (it % part_of_the_day == 0) {
+			char buf[1024];
+			nr0 = bv.norm(&psi0[0]);
+			nr1 = bv.norm(&psi1[0]);
+			if (isnan(nr0)) break;
+			if (isnan(nr1)) break;
+
+			fprintf(stderr, "nr0=%.16lf, nr1=%.16lf, t=%.16lf of %.16lf\n", nr0, nr1, t, T);
+			snprintf(buf, 1024, "_%06d", it);
+
+			output_psi("0_", buf, &psi0[0], nlat, nlon, U0, PSI0, grad);
+			output_psi("1_", buf, &psi1[0], nlat, nlon, U0, PSI0, grad);
+
+			vector < double > m0 = var0.m_current();
+			vector < double > d0 = var0.current();
+
+			vector < double > m1 = var1.m_current();
+			vector < double > d1 = var1.current();
+
+			output_psi("0_m_", "", &m0[0], nlat, nlon, U0, PSI0, grad);
+			output_psi("0_d_", "", &d0[0], nlat, nlon, U0, PSI0, grad);
+
+			output_psi("1_m_", "", &m1[0], nlat, nlon, U0, PSI0, grad);
+			output_psi("1_d_", "", &d1[0], nlat, nlon, U0, PSI0, grad);
+		}
+
+		bv.S_step (&psi0_n[0], &psi1_n[0], &psi0[0], &psi1[0], t);
+		t += conf.tau;
+
+		var0.accumulate(psi0_n);
+		var1.accumulate(psi1_n);
+
+		psi0.swap(psi0_n);
+		psi1.swap(psi1_n);
+
+		it += 1;
+	}
+
+	if (!isnan(nr0) && !isnan(nr1)) {
+		vector < double > m0 = var0.m_current();
+		vector < double > d0 = var0.current();
+
+		vector < double > m1 = var1.m_current();
+		vector < double > d1 = var1.current();
+
+		output_psi("0_m_", "", &m0[0], nlat, nlon, U0, PSI0, grad);
+		output_psi("0_d_", "", &d0[0], nlat, nlon, U0, PSI0, grad);
+
+		output_psi("1_m_", "", &m1[0], nlat, nlon, U0, PSI0, grad);
+		output_psi("1_d_", "", &d1[0], nlat, nlon, U0, PSI0, grad);
+	}
+}
+
 int main(int argc, char ** argv)
 {
 	fprintf(stderr, "#cmd:");
@@ -300,6 +533,7 @@ int main(int argc, char ** argv)
 	}
 	fprintf(stderr, "\n");
 	//set_fpe_except();
-	test_barvortex();
+	//test_convergence_barvortex();
+	real_calc(argv[1]);
 }
 
